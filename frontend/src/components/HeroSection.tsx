dd2e8bdc520bed56f9d02c803e81f00545216662
cf-lucas-moskau <54353309+cf-lucas-moskau@users.noreply.github.com>
2024-03-09 13:15:21 +0100

    text on frontpage

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -13,18 +13,26 @@ const HeroSection: React.FC = () => {
       flexDirection={"column"}
     >
       <Heading mb={4} size="2xl" fontWeight="bold" maxW={"1000px"}>
-        Join the Fast Track to Fitness and Fun with [Your Club Name]
+        Mikkeller Running Club Oslo
       </Heading>
       <Text fontSize="lg" maxW={"800px"} textAlign={"center"}>
-        At [Your Club Name], we're more than just a running club; we're a
-        community of enthusiasts and professionals dedicated to promoting
-        health, endurance, and the joy of running. Whether you're a seasoned
-        marathoner or a casual jogger, our club offers training sessions,
-        community runs, and events designed to support your fitness journey and
-        connect you with fellow runners in [Your Location]. With expert
-        coaching, tailored training programs, and a supportive environment,
-        [Your Club Name] is the perfect place to hit your stride and achieve
-        your running goals.
+        The idea of Mikkeller Running Club is to stay fit through running. That
+        makes us capable of enjoying even more of the good life – which includes
+        state of the art food and drinks. <br /> <br />
+        Obviously our main passion is beer – especially drinking beers – and
+        sometimes (preferably) in large scales.
+        <br />
+        <br /> The club is founded by Mikkel Borg Bjergsø, who is also the
+        creator and owner of Mikkeller. <br />
+        <br /> Speed is not the main target in Mikkeller Running Club. Having
+        fun and great social relations is our top priority and we're just proud
+        to see MRC members take part in races and events around the world.
+        <br />
+        <br /> You do not have to be an experienced runner or athlete to join
+        MRC…all we ask is for you to come join us and have fun staying healthy –
+        and sometimes a bit tipsy!!! <br />
+        <br />
+        We hope to see you wear the shirt around the world….
       </Text>
     </Box>
   );
